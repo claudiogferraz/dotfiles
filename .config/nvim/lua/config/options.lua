b5f4cedc31ad9eb26a6ef5d0a24a78e3f12535e7
Claudio G. Ferraz <contact@claudioferraz.com>
2025-06-09 23:54:24 -0300

    Add treesitter to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,10 +1,10 @@
--- Set tab size to 4 spaces globally
+-- Tabs options
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
--- Use spaces instead of tabs
 vim.opt.expandtab = true
--- Enable smart indentation (language-aware)
 vim.opt.smartindent = true
--- Enable auto indentation (copy indentation from previous line)
 vim.opt.autoindent = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
