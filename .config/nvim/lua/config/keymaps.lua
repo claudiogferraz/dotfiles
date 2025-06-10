acd0b6a948618ddb2301c0d9e204567a1b143f0b
Claudio G. Ferraz <contact@claudioferraz.com>
2025-06-10 18:55:31 -0300

    Update nvim plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
-return {
+vim.keymap.set('n', '<leader>n', ':bn<CR>', { desc = 'Go to next buffer'})
+vim.keymap.set('n', '<leader>p', ':bp<CR>', { desc = 'Go to previous buffer'})
+vim.keymap.set('n', '<leader>q', ':quit<CR>', { desc = 'Quit current window/buffer' })
 
-}
+vim.keymap.set('n', '<leader>t', ':terminal<CR>', { desc = 'Open terminal buffer' })
+
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
