5a3dcfa71e72baa9e1e4cea1a41e9f127a8c1bff
Claudio G. Ferraz <contact@claudioferraz.com>
2025-06-20 19:21:47 -0300

    Improved keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,29 +1,31 @@
 -- General keymaps
-vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { desc = "Open Lazy window" })
-vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Open Mason window" })
+vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Open Lazy window" })
+vim.keymap.set("n", "<leader>M", ":Mason<CR>", { desc = "Open Mason window" })
 vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { desc = "Open terminal buffer (ToggleTerm)" })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer (NvimTree)" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit INSERT mode on terminal" })
 vim.keymap.set("n", ":", ":FineCmdline<CR>", { desc = "Open FineCmdline" })
 
 -- Buffer keymaps
 vim.keymap.set("n", "<leader>b", "", { desc = "+Buffer options" })
-vim.keymap.set("n", "<leader>bn", ":bn<CR>", { desc = "Go to next buffer" })
-vim.keymap.set("n", "<leader>bp", ":bp<CR>", { desc = "Go to previous buffer" })
-vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Quit current buffer" })
-vim.keymap.set("n", "<leader>bd", ":bd<CR>:bp<CR>", { desc = "Quit current buffer + Go to previous" })
+vim.keymap.set("n", "<leader>n", ":bn<CR>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "<leader>p", ":bp<CR>", { desc = "Go to previous buffer" })
+vim.keymap.set("n", "<leader>d", ":bd<CR>:bp<CR>", { desc = "Quit current buffer" })
+vim.keymap.set("n", "<leader>D", ":bd!<CR>:bp<CR>", { desc = "Force quit current buffer" })
 
 -- Window keymaps
 vim.keymap.set("n", "<leader>w", "", { desc = "+Window options" })
-vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Focus window on the left" })
-vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Focus window on the bottom" })
-vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Focus window on the top" })
-vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Focus window on the right" })
-vim.keymap.set("n", "<leader>wq", ":q<CR>", { desc = "Quit current window" })
-vim.keymap.set("n", "<leader>wQ", ":qa<CR>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Focus window on the left" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Focus window on the bottom" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Focus window on the top" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus window on the right" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit current window" })
+vim.keymap.set("n", "<leader>Q", ":q!<CR>", { desc = "Force quit current window" })
+vim.keymap.set("n", "<leader>a", ":qa<CR>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>A", ":qa!<CR>", { desc = "Force quit all windows" })
 
 -- File related keymaps
 vim.keymap.set("n", "<leader>f", "", { desc = "File options" })
-vim.keymap.set("n", "<leader>fe", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer (NvimTree)" })
 vim.keymap.set("n", "<leader>fm", ":lua require(\"conform\").format({ async = true, lsp_format = \"fallback\" })<CR>",
   { desc = "Format file/buffer (Conform)" })
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files (Telescope)" })
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,6 +1,7 @@
 local lspconfig = require("lspconfig")
 
 require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "vtsls", "cssls", "tailwindcss", "prettier", "emmet_language_server", "rust_analyzer", "dockerls", "docker_compose_langauge_service" },
   handlers = {
     function(server_name)
       lspconfig[server_name].setup({})
